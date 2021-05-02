aaa50b024a0bedc5e7421ae042a732c622234fba
未日 <33367701+RealKiro@users.noreply.github.com>
2021-05-02 18:50:36 +0800

    remark useless

--- before_build.sh
+++ before_build.sh
@@ -15,16 +15,16 @@ sed -i 's/luci-theme-bootstrap/luci-theme-infinityfreedom/g' package/feeds/luci/
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/mine/luci-app-adguardhome
 
 # luci-app-bypass
-git clone https://github.com/garypang13/luci-app-bypass.git package/mine/luci-app-bypass
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
+# git clone https://github.com/garypang13/luci-app-bypass.git package/mine/luci-app-bypass
+# find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
+# find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
 
 # DiskMan for LuCI (WIP)
 # git clone https://github.com/lisaac/luci-app-diskman.git package/mine/luci-app-diskman
 # mkdir -p package/mine/parted && cp -i package/mine/luci-app-diskman/Parted.Makefile package/mine/parted/Makefile
 
 # luci-app-dnsfilter
-git clone https://github.com/garypang13/luci-app-dnsfilter.git package/mine/luci-app-dnsfilter
+# git clone https://github.com/garypang13/luci-app-dnsfilter.git package/mine/luci-app-dnsfilter
 
 # KPR
 # git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/mine/luci-app-koolproxyR
@@ -45,7 +45,7 @@ git clone https://github.com/garypang13/luci-app-dnsfilter.git package/mine/luci
 # git clone https://github.com/vernesong/OpenClash.git package/mine/OpenClash
 
 # tencent DDNS
-git clone https://github.com/Tencent-Cloud-Plugins/tencentcloud-openwrt-plugin-ddns.git package/mine/tencentcloud-openwrt-plugin-ddns
+# git clone https://github.com/Tencent-Cloud-Plugins/tencentcloud-openwrt-plugin-ddns.git package/mine/tencentcloud-openwrt-plugin-ddns
 
 # 网易云音乐
 # git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git package/mine/luci-app-unblockneteasemusic
