a71d3446da0df43c323e5e76c3d71683c1870df0
未日 <33367701+RealKiro@users.noreply.github.com>
2021-05-02 18:49:47 +0800

    change theme

--- before_build.sh
+++ before_build.sh
@@ -9,9 +9,7 @@ make defconfig
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认主题为Argon
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
-# git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/mine/luci-theme-argon_new
-# sed -i 's/luci-theme-bootstrap/luci-theme-argonv3/g' package/feeds/luci/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-infinityfreedom/g' package/feeds/luci/luci/Makefile
 
 # 复杂的AdGuardHome的openwrt的luci界面
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/mine/luci-app-adguardhome
